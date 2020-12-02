544d4236c62ffebf9d0873214be3dd3be00248e5
Luke Gartland <lukegartland@gmail.com>
2020-12-02 12:16:56 -1000

    finished styles and functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log('works');
+const navToggle = document.querySelector('.nav-toggle')
+const navLinks = document.querySelectorAll('.nav__link')
+
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open')
+})
+
+navLinks.forEach(link => link.addEventListener('click', () => {
+    console.log('ran');
+    document.body.classList.remove('nav-open')
+}))
